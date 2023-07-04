56003bbf9a0b1af79ca77bcbb6a326967071fd7c
Andres Gallego <andresfelipegallegozuleta@gmail.com>
2023-07-04 01:53:16 -0500

    changing url to fetch data

--- src/Hooks/useFetchCountriesData.js
+++ src/Hooks/useFetchCountriesData.js
@@ -16,8 +16,9 @@ function useFetchCountriesData(url) {
         async function getData() {
             try {
                 setLoadingStatus(LOADING_STATES[0]);
-                const response = await axios.get(url);
-                setData(response.data);
+                const response = await axios.get('https://api.github.com/repos/WalkerFGZ/rest_countries/git/blobs/1a74c81e637557f1de96b67912be777b4d28a31c');
+                const countryData = JSON.parse(atob(response.data.content))
+                setData(countryData);
                 setLoadingStatus(LOADING_STATES[1]);
             } catch (error) {
                 setError(error);
